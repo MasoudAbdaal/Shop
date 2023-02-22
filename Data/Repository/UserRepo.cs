c38c07bc89e7162836449879864510ff4c3f4bd8
MasoudAbdaal <m_abdaal@outlook.com>
2023-02-22 08:20:57 -0800

    Added .sln for VS

--- Data/Repository/UserRepo.cs
+++ Data/Repository/UserRepo.cs
@@ -7,94 +7,94 @@ using Shop.Models;
 namespace Shop.Data
 {
 
-  public class UserRepo : IUserRepo
-  {
-    private readonly MainContext _context;
+	public class UserRepo : IUserRepo
+	{
+		private readonly MainContext _context;
 
-    public UserRepo(MainContext context)
-    {
-      _context = context;
-    }
+		public UserRepo(MainContext context)
+		{
+			_context = context;
+		}
 
-    public Task SaveChanges()
-    {
-      return _context.SaveChangesAsync();
-    }
+		public Task SaveChanges()
+		{
+			return _context.SaveChangesAsync();
+		}
 
-    public async Task<User?> CreateUser(User user)
-    {
-      User? u = await GetUser(user.Email, null);
+		public async Task<User?> CreateUser(User user)
+		{
+			User? u = await GetUser(user.Email, null);
 
-      if (u == null)
-      {
-        await _context.Users.AddAsync(user!);
-        await SaveChanges();
-        return await _context.Users.FindAsync(user?.ID);
-      }
+			if (u == null)
+			{
+				await _context.Users.AddAsync(user!);
+				await SaveChanges();
+				return await _context.Users.FindAsync(user?.ID);
+			}
 
-      return u;
-    }
-
-
-    public async Task<User?> GetUser(string? email, byte[]? userId)
-    {
-      User? result;
-      if (userId != null)
-        result = await _context.Users.FindAsync(userId);
-      else
-        result = await _context.Users.FirstOrDefaultAsync(x => x.Email == email);
-
-      return result == null ? default : result;
-    }
-
-    public async Task<User?> EditUser(User user)
-    {
-      User? result;
-      if (user.Email != null)
-        result = await GetUser(user.Email, null);
-      else
-        result = await GetUser(null, user.ID);
+			return u;
+		}
 
 
+		public async Task<User?> GetUser(string? email, byte[]? userId)
+		{
+			User? result;
+			if (userId != null)
+				result = await _context.Users.FindAsync(userId);
+			else
+				result = await _context.Users.FirstOrDefaultAsync(x => x.Email == email);
 
-      if (result != null)
-      {
-        User? J = new User() { Name = "NEW USSSSSSSSSSSSER" };
+			return result == null ? default : result;
+		}
 
-        foreach (PropertyInfo item in user.GetType().GetProperties())
-        {
-          if (item.GetValue(user, null) != null)
-          {
+		public async Task<User?> EditUser(User user)
+		{
+			User? result;
+			if (user.Email != null)
+				result = await GetUser(user.Email, null);
+			else
+				result = await GetUser(null, user.ID);
 
-            Console.WriteLine("{0} ==>{1}", item.Name, item.GetValue(user, null) == null ? "NULL" : item.GetValue(user, null));
 
-            Console.WriteLine("{0}--------------{1}", J.GetType().GetProperty(item.Name)!.Name,
-            J.GetType().GetProperty(item.Name)!.GetValue(user, null)!);
 
-          }
+			if (result != null)
+			{
+				User? J = new User() { Name = "NEW USSSSSSSSSSSSER" };
 
-        }
+				foreach (PropertyInfo item in user.GetType().GetProperties())
+				{
+					if (item.GetValue(user, null) != null)
+					{
 
-        _context.Users.Update(result!);
-        await SaveChanges();
-        return await GetUser(null, result.ID);
-      }
+						Console.WriteLine("{0} ==>{1}", item.Name, item.GetValue(user, null) == null ? "NULL" : item.GetValue(user, null));
 
-      return default;
-    }
+						Console.WriteLine("{0}--------------{1}", J.GetType().GetProperty(item.Name)!.Name,
+						J.GetType().GetProperty(item.Name)!.GetValue(user, null)!);
 
-    public async Task<User?> DeleteUser(string email)
-    {
-      User? u = await GetUser(email, null);
-      if (u == null)
-        return default;
+					}
 
-      u!.UserInfo!.Enabled = false;
-      _context.Users.Update(u);
-      await SaveChanges();
+				}
 
-      return await GetUser(email, null);
-    }
-  }
+				_context.Users.Update(result!);
+				await SaveChanges();
+				return await GetUser(null, result.ID);
+			}
+
+			return default;
+		}
+
+		public async Task<User?> DeleteUser(string email)
+		{
+			User? u = await GetUser(email, null);
+			if (u == null)
+				return default;
+
+			u!.UserInfo!.Enabled = false;
+			_context.Users.Update(u);
+			await SaveChanges();
+
+			return await GetUser(email, null);
+		}
+	}
 
 }
